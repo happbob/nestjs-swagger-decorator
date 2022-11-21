57d1d6dd5ab6750113d1b7ebadb270c5471dc717
happbob <limms1217@naver.com>
2022-11-21 15:38:16 +0900

    22.11.21 version update

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-export * from './lib/decorator';
+import {ResponseT} from "./response.t";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
+import {applyDecorators} from "@nestjs/common";
+
+
+export const BaseSwaggerDecorator = (apiSummary: string, successResponseList: ResponseT[], validationList: ResponseT[]) => {
+    const successResponseDecorators = [], validationResponseDecorators = [];
+
+    for (let successData of successResponseList){
+        successData.type ?
+            successResponseDecorators.push(
+                ApiResponse({
+                    status: successData.code,
+                    description: successData.message,
+                    type: successData.type
+                })
+            ) :
+            successResponseDecorators.push(
+                ApiResponse({
+                    status: successData.code,
+                    description: successData.message,
+                })
+            )
+    }
+
+    for (let validationData of validationList){
+        validationResponseDecorators.push(
+            ApiResponse({
+                status: validationData.code,
+                description: validationData.message,
+            })
+        )
+    }
+
+    return applyDecorators(
+        ApiOperation({ summary: apiSummary }),
+        ...successResponseDecorators,
+        ...validationResponseDecorators,
+    )
+}
